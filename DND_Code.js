be4c63a3e13d38956b95ae63c38fc769d50232da
LDickman <dickmanl23@hanover.edu>
2021-09-30 19:37:51 -0400

    Corrected code, had the wrong id down in the js

--- DND_Code.js
+++ DND_Code.js
@@ -15,7 +15,7 @@ window.addEventListener('load', (event) => {
 /* When the user clicks on the button,
 toggle between hiding and showing the dropdown content */
 function raceSelect() {
-    document.getElementById("myDropdown").classList.toggle("show");
+    document.getElementById("raceDropdown").classList.toggle("show");
   }
 
 // Close the dropdown menu if the user clicks outside of it
