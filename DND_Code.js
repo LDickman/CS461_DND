21717f0d4320d84a7df0e336f777b7c276a03467
LDickman <dickmanl23@hanover.edu>
2021-10-05 10:05:42 -0400

    Got rid of the code that I did not need to make it neater

--- DND_Code.js
+++ DND_Code.js
@@ -48,16 +48,6 @@ function raceChoice() {
     });
 }
 
-// async function getInfo(eve) {
-//     console.log("Get result for dropdown bar");
-//     var text = document.getElementsByName('race');
-//     console.log("dropdown bar list "+ text);
-
-//     text.addEventListener('click', (event) => {
-//         raceAsk(document.querySelector('.dropdown-content ul li').value);
-//     });
-
-// }
 
 async function setup() {
     console.log("starting");
@@ -75,30 +65,15 @@ async function raceAsk(input) {
     console.log(url);
     const response = await fetch(url);
     const data = await response.json();
-    printData(data);   //--- kept geeting error with function refereance
-    // if (input != null) {
-    //     console.log(input);
-    //     const url = api + input + apiKey;
-    //     console.log(url);
-    //     const response = await fetch(url);
-    //     const data = await response.json();
-    //     printData(data);
-    // }
-    //console.log("asking");
+    printData(data);   
 }
 
-// export function gotData(data) {
-//     //println(data);
-//     races = data;
-// }
 
 async function printData(data) {
     console.log(data);
     const { count, results } = data;
     console.log("Count: " + count);
     console.log("Results: " + results);
-    // const resultArray = Object.values(results);
-    //document.querySelector("#results").textContent = results.toString();
     var String_Array = results.map(function (el) {
         return el.name;
     });
@@ -106,44 +81,10 @@ async function printData(data) {
         return el.url;
     });
     console.log(url_Array);
-    //askTraits(url_Array);
-    // var des_Array = url_Array.map( function( el ) {
-    //     console.log(el.desc);
-    //     return el.desc;
-    // });
-    //console.log(des_Array);
     document.querySelector("#results").textContent = String_Array.join(',     ');
-    //document.querySelector("#des").textContent = des_Array.join(',     ');
     document.querySelector('#count').textContent = count;
     console.log("printing");
 }
-
-// async function askTraits(url) {
-//     console.log(url);
-//     // for (i = 0; i < url.length; i++){
-//     // }
-//     const response = await fetch(url);
-//     const data = await response.json();
-//     printInfo(data);
-// }
-
-// async function printInfo(data) {
-//     var des_Array = url_Array.map( function( el ) {
-//         console.log(el.desc);
-//         return el.desc;
-//     });
-//     console.log(des_Array);
-//     document.querySelector("#des").textContent = des_Array.join(',     ');
-// }
-
-// const api_url = 'https://www.dnd5eapi.co/api/races/human/?ability_bonuses';
-
-// export function getSizeDes() {
-//     const response = await fetch(api_url);
-//     const data = await response.json();
-//     const {size} = data;
-//     document.getElementById('size_description').textContent = size;
-// }
 
 function getOneAbilityScores() {
     return Math.floor(Math.random() * 15);
