9a5a47dded6eb29357fd9e73c26835bc6013d354
LDickman <dickmanl23@hanover.edu>
2022-01-05 14:47:02 -0500

    Added more tests for the roll functions from the ability_score js

--- tests/example_problems.js
+++ tests/example_problems.js
@@ -26,10 +26,41 @@ function ifUrlExist(url) {
   } else if (request.status != 404) {
     return true;
   }
-  // request.send();
-  // if (request.status === 404) {
-  //     return false;
-  // } else {
-  //   return true;
-  // }
-};
+}
+
+/// Functions from the ability score page for rolling the dice
+function rollsForScore(totalRolls) {
+  var sum = totalRolls.reduce((a, b) => a + b, 0)
+  return (sum);
+}
+
+function calcForAbilityScoreModifier(modifier, score) {
+  switch (score) {
+      case 3:
+          return (modifier - 4);
+      case 4:
+      case 5:
+          return (modifier - 3);
+      case 6:
+      case 7:
+          return (modifier - 2);
+      case 8:
+      case 9:
+          return (modifier - 1);
+      case 10:
+      case 11:
+          return (modifier + 0);
+      case 12:
+      case 13:
+          return (modifier + 1);
+      case 14:
+      case 15:
+          return (modifier + 2);
+      case 16:
+      case 17:
+          return (modifier + 3);
+      case 18:
+          return (modifier + 4);
+  }
+}
+/// End of Functions from the ability score page for rolling the dice
--- tests/tests_code.js
+++ tests/tests_code.js
@@ -22,7 +22,43 @@ describe("Verifying that strings are actual web app links and the links are avai
   });
 });
 
-
+// EX: rolls: 1, 3, 4   Ability score is 8
+describe("Fucntions within the ability Score tab: rollsForScore, and calcForAbilityScoreModifier", () => {
+  it("rollOneDice: randomly rolls a number", () => {
+    expect(isValidUrl("https://www.dnd5eapi.co/api/classes/bard")).to.be.true;
+    expect(isValidUrl("https://www.dnd5eapi.co/api/classes/monk")).to.be.true;
+    expect(isValidUrl("https://www.dnd5eapi.co/api/races/human")).to.be.true;
+    expect(isValidUrl("https://www.dnd5eapi.co/api/races/elf")).to.be.true;
+  });
+  it("rollsForScore: adds three scores together", () => {
+    expect(rollsForScore([1, 3, 4])).to.deep.equal(8);
+    expect(rollsForScore([6, 5, 4])).to.deep.equal(15);
+    expect(rollsForScore([4, 3, 4])).to.deep.equal(11);
+    expect(rollsForScore([1, 1, 1])).to.deep.equal(3);
+  });
+  it("calcForAbilityScoreModifier: with modifier of 0 should result in a certain number", () => {
+    expect(calcForAbilityScoreModifier(0, 3)).to.deep.equal(-4);
+    expect(calcForAbilityScoreModifier(0, 4)).to.deep.equal(-3);
+    expect(calcForAbilityScoreModifier(0, 7)).to.deep.equal(-2);
+    expect(calcForAbilityScoreModifier(0, 9)).to.deep.equal(-1);
+    expect(calcForAbilityScoreModifier(0, 10)).to.deep.equal(0);
+    expect(calcForAbilityScoreModifier(0, 13)).to.deep.equal(1);
+    expect(calcForAbilityScoreModifier(0, 14)).to.deep.equal(2);
+    expect(calcForAbilityScoreModifier(0, 16)).to.deep.equal(3);
+    expect(calcForAbilityScoreModifier(0, 18)).to.deep.equal(4);
+  });
+  it("calcForAbilityScoreModifier: with modifier of greater than 0 should result in a certain number", () => {
+    expect(calcForAbilityScoreModifier(2, 3)).to.deep.equal(-2);
+    expect(calcForAbilityScoreModifier(1, 4)).to.deep.equal(-2);
+    expect(calcForAbilityScoreModifier(1, 7)).to.deep.equal(-1);
+    expect(calcForAbilityScoreModifier(2, 9)).to.deep.equal(1);
+    expect(calcForAbilityScoreModifier(1, 10)).to.deep.equal(1);
+    expect(calcForAbilityScoreModifier(2, 13)).to.deep.equal(3);
+    expect(calcForAbilityScoreModifier(1, 14)).to.deep.equal(3);
+    expect(calcForAbilityScoreModifier(2, 16)).to.deep.equal(5);
+    expect(calcForAbilityScoreModifier(1, 18)).to.deep.equal(5);
+  });
+});
 
 
 //   it("Array of one element, that returns that elemnt", () => {
